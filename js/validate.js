cf222efb80966ead655845988be2f32876abf41a
Garian <garianchung@gmail.com>
2025-12-30 21:49:10 +0800

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -25,6 +25,8 @@ function validateForm() {
         formOK = false;
         document.getElementById("name_err").innerHTML = "Name must only be alphabetical values";
     }
+
+    return formOK;
 }
 
 function validateMobile(str) {
@@ -44,4 +46,5 @@ function clearErrorMsgs() {
         labels[i].innerHTML = "";
     }
 }
+
 
